21019447288d4434b03896ed80388d463f6ede38
Rodipoy <nik.telenchy@gmail.com>
2022-07-09 22:36:24 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function onmentr(){
+    img = document.getElementById('img')
+    img.src = '/img/chat_green.svg'
+}
+
+function onmlev(){
+    img = document.getElementById('img')
+    img.src = '/img/chat_white.svg'
+}
